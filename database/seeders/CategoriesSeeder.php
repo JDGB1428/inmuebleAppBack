fe199f131634dc0f4204d6532d9a7fa1709c9c61
JDGB1428 <juangalindo28@hotmail.com>
2026-02-03 17:49:37 -0500

    Creacion de las categorias para la tabla categorias

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -3,8 +3,7 @@
 namespace Database\Seeders;
 
 use Carbon\Carbon;
-use Illuminate\Container\Attributes\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class CategoriesSeeder extends Seeder
@@ -14,11 +13,31 @@ class CategoriesSeeder extends Seeder
      */
     public function run(): void
     {
-        // DB::table('categorias')->insert([
-        //     'nombre' => 'Galletas',
-        //     'icono' => 'galletas',
-        //     'created_at' => Carbon::now(),
-        //     'updated_at' => Carbon::now(),
-        // ]);
+        DB::table('categories')->insert([
+            [
+                'name' => 'House',
+                'icon' => 'house',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Apartment',
+                'icon' => 'apartment',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Land',
+                'icon' => 'land',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Office',
+                'icon' => 'office',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
     }
 }
