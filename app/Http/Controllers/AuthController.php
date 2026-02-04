6a8b4fe8e08c646c52e97f6438729ead9262d8a6
JDGB1428 <juangalindo28@hotmail.com>
2026-02-04 12:20:22 -0500

    creacion de las funciones login y register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
 
-    public function login(){
+    public function login(LoginRequest $request){
 
     }
 
-    public function register(){
+    public function register(RegisterRequest $request): array{
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'message' => response()->json($user, 201),
+            'user' => $user
+        ];
 
     }
 }
