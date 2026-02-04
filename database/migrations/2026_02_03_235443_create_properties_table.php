43075e1976447b6ff9e41651f1ed443a001b9bb2
JDGB1428 <juangalindo28@hotmail.com>
2026-02-03 19:18:12 -0500

    creacion de la columnas de la tabla properties

---
+++ database/migrations/2026_02_03_235443_create_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price');
+            $table->string('direction');
+            $table->integer('room');
+            $table->integer('area');
+            $table->integer('bathrooms');
+            $table->enum('state', ['available', 'sold', 'rented'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
