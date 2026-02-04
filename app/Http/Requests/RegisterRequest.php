90ee90b3e0404e3f29c8258da84dc7103f17421d
JDGB1428 <juangalindo28@hotmail.com>
2026-02-04 12:05:05 -0500

    creacion de las regals de validacion delregistro de usuario

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password as PasswordRules;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','string'],
+            'email' => ['required','email','unique:users,email'],
+            'password' => ['required','confirmed', PasswordRules::min(8)->numbers()->letters()->symbols()],
+        ];
+    }
+
+    public function messages(): array {
+        return [
+            'name.required' => 'El nombre es obligatorio',
+            'email.required' => 'El correo electrónico es obligatorio',
+            'email.email' => 'El correo electrónico no es válido',
+            'email.unique' => 'El correo electrónico ya está en uso',
+            'password.required' => 'La contraseña es obligatoria',
+            'password.confirmed' => 'Las contraseñas no coinciden',
+            'password.min' => 'La contraseña debe tener al menos 8 caracteres',
+            'password.numbers' => 'La contraseña debe contener al menos un número',
+            'password.letters' => 'La contraseña debe contener al menos una letra',
+            'password.symbols' => 'La contraseña debe contener al menos un símbolo',
+        ];
+    }
+}
