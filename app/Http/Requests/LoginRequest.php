529acca78c0ba21dfa5da54c2335f722c94a7992
JDGB1428 <juangalindo28@hotmail.com>
2026-02-05 14:15:23 -0500

    Agregando validaciones para el logueo del usuario

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -11,7 +11,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,18 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email' => ['required','email', 'exists:users,email'],
+            'password' => ['required'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'El correo electrónico es obligatorio',
+            'email.email' => 'El correo electrónico no es válido',
+            'email.exists' => 'El correo electrónico no está registrado',
+            'password.required' => 'La contraseña es obligatoria',
         ];
     }
 }
