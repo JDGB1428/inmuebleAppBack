59e38ead7419ea27543294a880cf0549dde63268
JDGB1428 <juangalindo28@hotmail.com>
2026-02-06 16:04:59 -0500

    Agregando middleware de roles y persmisos. agregando exception si no cumple con el permiso adecuado

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +13,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function(UnauthorizedException $e, $request) {
+            if($request->is('api/*')){
+                return response()->json([
+                    'message' => 'No tienes permiso para realizar esta accion'
+                ], 403);
+            }
+        });
     })->create();
