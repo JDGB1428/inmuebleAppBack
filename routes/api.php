95e6275c0b58cbc148da758a6a381f7ee56e7f4c
JDGB1428 <juangalindo28@hotmail.com>
2026-02-06 16:03:55 -0500

    Creando un grupo de rutas con middleware, teniendo en cuenta sus roles y permisos

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PropertyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,19 @@ Route::middleware('auth:sanctum')->group(function (){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::group(['middleware' => [
+        'role:admin|agent',
+        'permission:
+            ver inmuebles,
+            crear inmuebles,
+            editar inmuebles,
+            eliminar inmuebles']],
+            function(){
+                Route::get('/property',[PropertyController::class, 'index']);
+                Route::post('/property', [PropertyController::class, 'store']);
+    });
+
 });
 
 Route::post('/login', [AuthController::class, 'login']);
