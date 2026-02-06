fc2bedaa3c7c89385790313aa1596acd26d97983
JDGB1428 <juangalindo28@hotmail.com>
2026-02-06 16:03:12 -0500

    Creacion de permisos y roles y assignando roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_admin = Role::create(['name' => 'admin']);
+        $role_client = Role::create(['name' => 'client']);
+        $role_agent = Role::create(['name' => 'agent']);
+
+        // Inmueble
+        Permission::create(['name' => 'ver inmuebles'])->assignRole($role_client);
+        Permission::create(['name' => 'crear inmuebles'])->syncRoles([$role_agent, $role_admin]);
+        Permission::create(['name' => 'editar inmuebles'])->syncRoles([$role_agent, $role_admin]);
+        Permission::create(['name' => 'eliminar inmuebles'])->syncRoles([$role_agent, $role_admin]);
+
+        // Usuarios
+        Permission::create(['name' => 'ver usuarios'])->assignRole($role_admin);
+        Permission::create(['name' => 'banear usuarios'])->assignRole($role_admin);
+        Permission::create(['name' => 'crear usuarios'])->assignRole($role_admin);
+        Permission::create(['name' => 'editar usuarios'])->assignRole($role_admin);
+
+
+    }
+}
