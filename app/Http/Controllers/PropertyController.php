ae85ce9d26f1725847f6a3eef4793f526bba9c9e
JDGB1428 <juangalindo28@hotmail.com>
2026-02-06 16:05:20 -0500

    Creando nuevo controlador de property

---
+++ app/Http/Controllers/PropertyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PropertyController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(){
+
+    }
+}
