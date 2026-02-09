c2564471393772c923d540d247986686d0672516
JDGB1428 <juangalindo28@hotmail.com>
2026-02-09 16:01:14 -0500

    Creando la funcion updateProfile

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PropertyRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProfileController extends Controller
+{
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'avatar' => ['required', 'image', 'mimes:jpeg,png,jpg', 'max:2048']
+        ]);
+
+        $user = auth()->user();
+
+        if ($request->hasFile('avatar')) {
+            if ($user->avatar) {
+                $oldPath = str_replace('/storage/', '', $user->avatar);
+                Storage::disk('public')->delete($oldPath);
+            }
+            $path = $request->file('avatar')->store('avatars', 'public');
+            $user->avatar = Storage::url($path);
+            $user->save();
+        }
+
+        return response()->json([
+            'message' => 'Avatar actualizado',
+            'avatar_url' => $user->avatar
+        ]);
+    }
+}
