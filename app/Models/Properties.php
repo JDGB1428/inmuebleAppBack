4d782cb515f7943d2ae0c96a96f0a044def663f8
JDGB1428 <juangalindo28@hotmail.com>
2026-02-09 13:16:18 -0500

    Implementando belongTo (OneToMany de manera inversa)

--- app/Models/Properties.php
+++ app/Models/Properties.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Properties extends Model
 {
@@ -16,5 +17,14 @@ class Properties extends Model
         'bathrooms',
         'state'
     ];
+
+
+    public function categories(): BelongsTo {
+        return $this->belongsTo(Categories::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 
 }
