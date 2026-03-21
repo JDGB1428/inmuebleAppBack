bbd7a1be624604d70bf5b9c89b39a6d98eaa5ae0
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 13:57:55 -0500

    Documentando controller con swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="API de Inmuebles",
+ *     version="1.0",
+ *     description="Documentación de la aplicación de Inmuebles"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ */
 abstract class Controller
 {
-    //
+    /**
+     * @OA\Get(
+     *     path="/api/test",
+     *     summary="Endpoint de prueba para Swagger",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Operación exitosa"
+     *     )
+     * )
+     */
+    public function test()
+    {
+        return response()->json(['status' => 'ok']);
+    }
 }
