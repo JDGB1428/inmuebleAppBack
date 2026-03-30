dcd7adc3ec833fff7c6b8f646ba8311e80bae5cd
JDGB1428 <juangalindo28@hotmail.com>
2026-03-30 10:23:58 -0500

    Funciones para hacer la relacion de uno a muchos de manera inversa

--- app/Models/Commentary.php
+++ app/Models/Commentary.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Commentary extends Model
 {
@@ -15,4 +16,14 @@ class Commentary extends Model
         'user_id',
         'description'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function property(): BelongsTo
+    {
+        return $this->belongsTo(Properties::class);
+    }
 }
