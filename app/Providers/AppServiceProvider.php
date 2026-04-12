17bd2016e78395faba99dc8199d8b9839627de6d
JDGB1428 <juangalindo28@hotmail.com>
2026-04-12 10:35:50 -0500

    Aplicando servicios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Events\PropertyLiked;
+use App\Events\PropertyStatusChanged;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
 use App\Events\PropertyStored;
 use App\Listeners\NotifyUsersOfPropertyStatusChange;
+use App\Listeners\SendPropertyLikedNotificationToOwner;
+use App\Listeners\SendPropertyNotificationsToClients;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +28,19 @@ class AppServiceProvider extends ServiceProvider
     {
         Event::listen(
             PropertyStored::class,
+            SendPropertyNotificationsToClients::class
+        );
+
+        // 2. Cuando CAMBIA DE ESTADO (Vendida, etc) -> Avisar a los que le dieron Like
+        Event::listen(
+            PropertyStatusChanged::class,
             NotifyUsersOfPropertyStatusChange::class
+        );
+
+        // 3. Cuando alguien le da LIKE -> Avisar al dueño (El que creamos hoy)
+        Event::listen(
+            PropertyLiked::class,
+            SendPropertyLikedNotificationToOwner::class
         );
     }
 }
